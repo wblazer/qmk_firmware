c329f028a1369266f8db52dabbc143427b932899
William Blazer <82427064+wblazer@users.noreply.github.com>
2025-02-22 20:48:02 -0600

    Updates

--- keyboards/boardsource/lulu/keymaps/wblazer/keymap.c
+++ keyboards/boardsource/lulu/keymaps/wblazer/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  =   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | LGUI | LALT |
+ *                   | LGUI | LALT |LOWER | /Space  /       \Enter \  |RAISE | LALT | LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -33,31 +33,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   QK_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQUAL,
-                          KC_LALT,    KC_LGUI, LOWER, KC_SPC,  KC_ENT,   RAISE,   KC_LGUI, KC_LALT
+  KC_LCTL,  HOME_A, HOME_S,  HOME_D,  HOME_F,  KC_G,                    KC_H,     HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQUAL,
+                          KC_LGUI,    KC_LALT, LOWER, KC_SPC,  KC_ENT,   RAISE,   KC_LALT, KC_LGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      | Del  |
+ * |      |      |      |      |      |      |                    |      |      |      |  [   |   ]  | Del  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------| Left | Down |  Up  | Right|      |  ~   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * | CAPS |      |      |      |      |      |-------|    |-------|      |   {  |   }  |   [  |   ]  |   |  |
+ * | CAPS |      |      |      |      |      |-------|    |-------|      |   {  |   }  |      |   \  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | LGUI | LALT |
+ *                   | LGUI | LALT |LOWER | /Space  /       \Enter \  |RAISE | LALT | LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_LBRC, KC_RBRC, KC_DEL,
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_TILD,
-  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE,
+  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, KC_BSLS, KC_PIPE,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
+
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | Mute | Vol- | Vol+ | Play | Prev | Next |                    |  MOD |  S+  |  S-  |  B+  |  B-  |RGBTOG|
@@ -66,9 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   +  |   -  |      |      |   \  |
+ * |      |      |      |      |      |      |-------|    |-------|      | PGDN | PGUP |      |      |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | LGUI | LALT |
+ *                   | LGUI | LALT |LOWER | /Space  /       \Enter \  |RAISE | LALT | LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -77,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT,                   RGB_MOD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_TOG,
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_MINS, _______, _______, KC_BSLS,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, KC_BSLS,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* ADJUST
@@ -90,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE | LGUI | LALT |
+ *                   | LGUI | LALT |LOWER | /Space  /       \Enter \  |RAISE | LALT | LGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
--- keyboards/boardsource/lulu/rp2040/config.h
+++ keyboards/boardsource/lulu/rp2040/config.h
@@ -9,3 +9,25 @@
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP22
 #define I2C1_SCL_PIN GP23
+
+// Fixes keyboard not working on boot
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 3000
+
+// Mod tap settings:
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN LGUI_T(KC_SCLN)
+
+#define TAPPING_TERM 180
+// Tap and hold mod tap key, if faster than this timer, hold modifier - else, repeat key
+#define QUICK_TAP_TERM 80
+// #define PERMISSIVE_HOLD
