35cb16e96901d96b34013ac998d9cf3ae3b5ccca
William Blazer <82427064+wblazer@users.noreply.github.com>
2025-05-23 12:36:30 -0500

    Piantor pro updates

--- keyboards/beekeeb/piantor_pro/keymaps/wblazer/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/wblazer/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       QK_GESC,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,                         KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, LOWER,  KC_SPC,     KC_ENT,   SYM, KC_BSPC
                                       //`--------------------------'  `--------------------------'
@@ -42,11 +42,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_HASH,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_CIRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         // KC_AT, KC_PLUS, KC_MINS, KC_UNDS,  KC_EQL, KC_TILD,                      KC_GRV, KC_EXLM, KC_ASTR, KC_AMPR, KC_PIPE, KC_BSLS,
-         KC_AT, KC_EXLM, KC_PLUS, KC_UNDS,  KC_EQL, KC_TILD,                      KC_GRV, KC_MINS, KC_AMPR, KC_PIPE, KC_ASTR, KC_BSLS,
+         // KC_AT, KC_EXLM, KC_PLUS, KC_UNDS,  KC_EQL, KC_TILD,                      KC_GRV, KC_MINS, KC_AMPR, KC_PIPE, KC_ASTR, KC_BSLS,
+         KC_AT, KC_EXLM, KC_PLUS, KC_UNDS,  KC_EQL, KC_TILD,                      KC_GRV, KC_MINS,   KC_LT,   KC_GT, KC_ASTR, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       // _______,   KC_LT, KC_LBRC, KC_LCBR, KC_LPRN, KC_HASH,                      KC_DLR, KC_RPRN, KC_RCBR, KC_RBRC,   KC_GT, KC_PERC,
-       _______,  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_LT,                      KC_GT, KC_LCBR, KC_RCBR, KC_DOT,   KC_DLR, KC_PERC,
+       _______,  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,KC_AMPR,                     KC_PIPE, KC_LCBR, KC_RCBR, KC_DOT,   KC_DLR, KC_PERC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,     _______, _______, _______
                                       //`--------------------------'  `--------------------------'
--- keyboards/boardsource/lulu/keymaps/wblazer/keymap.c
+++ keyboards/boardsource/lulu/keymaps/wblazer/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   QK_GESC,  HOME_A, HOME_S,  HOME_D,  HOME_F,  KC_G,                     KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQUAL,
-                          KC_LGUI,    KC_LALT, LOWER, KC_SPC,  KC_ENT,   SYM,     KC_BSPC, KC_LGUI
+                             KC_LGUI, KC_LGUI, LOWER,  KC_SPC,     KC_ENT,   SYM, KC_BSPC, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   KC_HOME, _______, _______, KC_END,  KC_BSPC, KC_DEL,
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC, KC_DEL,
-    KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, KC_BSLS, KC_PIPE,
+    KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,
                                 _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SYM] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CIRC,
-    KC_AT,   KC_PLUS, KC_MINS, KC_UNDS, KC_EQL,  KC_TILD,                   KC_GRV,  KC_EXLM, KC_ASTR, KC_AMPR, KC_PIPE, KC_BSLS,
-    _______, KC_LT,   KC_LBRC, KC_LCBR, KC_LPRN, KC_HASH, _______, _______, KC_DLR,  KC_RPRN, KC_RCBR, KC_RBRC, KC_GT,   KC_PERC,
+    KC_HASH, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CIRC,
+    KC_AT,   KC_EXLM, KC_PLUS, KC_UNDS, KC_EQL,  KC_TILD,                   KC_GRV, KC_MINS, KC_AMPR, KC_PIPE, KC_ASTR, KC_BSLS,
+    _______,  KC_LBRC,KC_RBRC, KC_LPRN, KC_RPRN, KC_LT, _______, _______,   KC_GT, KC_LCBR, KC_RCBR, KC_DOT,   KC_DLR, KC_PERC,
                                 _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
     KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT,                   RGB_MOD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_TOG,
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F12,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,
                                 _______, _______, _______, _______, _______, _______, _______, _______
 )
